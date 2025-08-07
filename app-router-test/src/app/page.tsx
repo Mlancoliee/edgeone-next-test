01d6f27a039da016dcd973dec72fa8170b5f4c5f
jesper <550763365@qq.com>
2025-08-07 13:05:48 +0800

    link to about

--- app-router-test/src/app/page.tsx
+++ app-router-test/src/app/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>App Router Test Verified</h1>
+    <>
+      <h1>App Router Test Verified</h1>
+      <Link href="/about"></Link>
+    </>
   );
 }
