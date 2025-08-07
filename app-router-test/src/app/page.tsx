cc72a0e708d92524695b51fb222961143fc314ee
jesper <550763365@qq.com>
2025-08-07 17:59:54 +0800

    form

--- app-router-test/src/app/page.tsx
+++ app-router-test/src/app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <>
       <h1>App Router Test Verified</h1>
-      <Link href="/about"></Link>
+      <Link href="/about">About</Link>
+      <Link href="/component">About</Link>
     </>
   );
 }
